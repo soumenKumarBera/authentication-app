64626fc2c4a521fb23c92cdc2ffaf3f85a809cf3
soumen kumar bera <soumenbera4017@gmail.com>
2026-05-06 16:58:29 +0530

    setup Authenticatiion app

---
+++ src/main/java/in/kb/main/AuthenticationAppApplication.java
@@ -0,0 +1,13 @@
+package in.kb.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationAppApplication.class, args);
+	}
+
+}
